8604d9c645e0b54cd43d885bc2ba08060f4e0e08
alessandra martera <ale.martera@gmail.com>
2023-06-30 16:15:36 +0200

    update function

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <?php
-    class Date{
-        public $day;
-        public $month;
-        public $year;
+    // class FilmDate {
+    //     public $day;
+    //     public $month;
+    //     public $year;
 
-        public function __construct($day, $month, $year){
-            $this -> day = $day;
-            $this -> month = $month;
-            $this -> year = $year;
-        }
-    }
+    //     public function __construct($day, $month, $year){
+    //         $this -> day = $day;
+    //         $this -> month = $month;
+    //         $this -> year = $year;
+    //     }
+    // }
 
     class Movie {
         public $name;
         public $language;
         public $type;
-        public Date $date;
+        public $FilmDate;
 
-        public function __construct($name, $language, $type, Date $date) {
+        public function __construct($name, $language, $type, $FilmDate) {
             
             $this -> name = $name;
             $this -> language = $language;
             $this -> type = $type;
-            $this -> date = $date;
+            $this -> FilmDate = $FilmDate;
         }
 
         public function getmovie() {
-            return  $this -> $date . " " . $this -> $date;
+            return  $this -> name . " => " . $this -> language . ", " .  $this -> type . ", " . $this -> FilmDate;
         }
 
-        public getDate(){
-
-        }
+        
     }
 
-    $movie1 = new movie("ritorno al futuro", "en", "action", $data1);
-    $data1 = new date( "27", "october", "1985");
+    $movie1 = new movie ("ritorno al futuro", "en", "action", "11 Apr 2019");
+    // $data1 = new FilmDate ( "27", "october", "1985");
 
-    $movie2 = new movie("Endgame", "en", "fantasy", "11 Apr 2019", $data2);
-    $data2 = new date( "11", "april", "2019");
+    $movie2 = new movie ("Endgame", "en", "fantasy", "11 Apr 2019");
+    // $data2 = new FilmDate ( "11", "april", "2019");
 
     // var_dump($movie1, $movie2);
     // echo "</br>" . $movie1 -> name;
